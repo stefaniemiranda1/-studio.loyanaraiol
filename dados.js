6c9f32c33c1102d27dc9bede7bb8059d6845e4c7
stefaniemiranda1 <stefaniedmiranda@gmail.com>
2024-09-14 19:41:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,15 @@
+const botao1 = document.getElementById("botao1");
+const botao2 = document.getElementById("botao2");
+const botao3 = document.getElementById("botao3");
+
+botao1.addEventListener("click", function() {
+  window.open("https://www.instagram.com/studio.loyanaraiol/", "_blank"); // instagram
+});
+
+botao2.addEventListener("click", function() {
+  window.open("https://api.whatsapp.com/message/GZTSGRGR6UJ3O1?autoload=1&app_absent=0", "_blank"); // whats
+});
+
+botao3.addEventListener("click", function() {
+  window.open("https://maps.app.goo.gl/dHPNVxtY11nHFhB26", "_blank"); //endereço
+});
